76af79a20f566dc0d2e7b8c2bfccd9f4d0b67749
ESKYoung <ESKYoung@users.noreply.github.com>
2022-09-05 08:26:17 +0100

    test: add `nox` session to build an example project, and test it

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,7 @@
 """Define ``nox`` sessions."""
 
 import os
+from tempfile import TemporaryDirectory
 from typing import Iterable, Optional
 
 import nox
@@ -111,3 +112,63 @@ def build_and_test_sphinx_documentation(nox_session: Session, builder: str) -> N
     docs_build_directory = os.path.join(nox_session.env["TMPDIR"], "docs/_build")
     args = nox_session.posargs or ["docs", docs_build_directory]
     nox_session.run("sphinx-build", "-b", builder, *args, external=True)
+
+
+@session(name="_example", python=PYTHON_VERSIONS)
+def build_example_project(nox_session: Session) -> None:
+    """Build an example project to test it has been developed created.
+
+    Args:
+        nox_session (Session): a ``nox_poetry.Session`` object.
+
+    Returns:
+        None.
+
+    """
+    install_group_dependencies(nox_session=nox_session)
+
+    # Create a temporary directory, where an example project will be build for testing
+    # purposes. This is required, as the example project needs Git initialised, and it
+    # cannot be inside an existing Git repository
+    with TemporaryDirectory() as temporary_directory:
+
+        example_project_repository = os.path.join(
+            temporary_directory, "example-project"
+        )
+        example_project_requirements = os.path.join(
+            example_project_repository,
+            f"requirements-{nox_session.name}.txt",
+        )
+
+        # Run `cookiecutter` using this project to create an example project called
+        # "Example Project"; use the default values for all other inputs
+        nox_session.run(
+            "cookiecutter",
+            f"--output-dir={temporary_directory}",
+            "--no-input",
+            os.getcwd(),
+            "project_name=Example Project",
+        )
+
+        # Change the current working directory for the `nox` session
+        nox_session.chdir(example_project_repository)
+
+        # Remove any Python packages in the current `nox` session, and install the
+        # example project main dependencies
+        with open(example_project_requirements, "w") as f:
+            f.write(str(nox_session.run("pip", "freeze", silent=True, external=True)))
+        nox_session.run(
+            "pip",
+            "uninstall",
+            "--yes",
+            "--quiet",
+            f"--requirement={example_project_requirements}",
+        )
+        install_group_dependencies(nox_session=nox_session)
+
+        # Initialise Git in the example project, and stage all changes
+        nox_session.run("git", "init", "--quiet", external=True)
+        nox_session.run("git", "add", ".", external=True)
+
+        # Run the example project `nox` sessions
+        nox_session.run("nox", external=True)
