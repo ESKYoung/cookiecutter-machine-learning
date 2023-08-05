d7fbfda0e528afbc20162ff2a2462b3bef136a1b
ESKYoung <ESKYoung@users.noreply.github.com>
2023-08-05 17:51:31 +0100

    ci: integrate `nox_poetry` 1.0.3 group dependency builds

--- {{ cookiecutter.repository_name }}/noxfile.py
+++ {{ cookiecutter.repository_name }}/noxfile.py
@@ -1,7 +1,6 @@
 """Define ``nox`` sessions."""
 
 import os
-from typing import Iterable, Optional
 
 import nox
 from nox_poetry import Session, session
@@ -10,47 +9,10 @@ from nox_poetry import Session, session
 PYTHON_VERSIONS = ["3.9", "3.10"]
 
 # Set minimum required version of `nox`
-nox.needs_version = ">=2022.8.7"
+nox.needs_version = ">=2023.4.22"
 
 # Set `nox` to stop the session on the first failure
 nox.options.stop_on_first_error = True
-
-
-def install_group_dependencies(
-    nox_session: Session,
-    groups: Optional[Iterable[str]] = None,
-) -> None:
-    """Install Poetry group dependencies with the root package.
-
-    Args:
-        nox_session (Session): a ``nox_poetry.Session`` object.
-        groups (Optional[Iterable[str]], optional): an iterable of group names from
-            ``pyproject.toml``, which contain packages required for this session. If
-            ``None``, only the ``ci-cd`` dependency group will be installed.
-
-    Returns:
-        None.
-
-    """
-    # Create a temporary folder to store a requirements file of the root package, its
-    # main dependencies, and specified group dependencies
-    temporary_directory_path = nox_session.create_tmp()
-    temporary_requirements = os.path.join(
-        temporary_directory_path,
-        f"requirements-{nox_session.name}.txt",
-    )
-
-    # Export specific groups to a `requirements.txt` file, and then install them
-    nox_session.run_always(
-        "poetry",
-        "export",
-        "--format=requirements.txt",
-        f"--output={temporary_requirements}",
-        "--with={}".format(",".join({"ci-cd", *groups}) if groups else "ci-cd"),
-        external=True,
-    )
-    nox_session.install("-r", str(temporary_requirements))
-    nox_session.poetry.installroot()
 
 
 @session(
@@ -67,10 +29,7 @@ def run_pre_commit_hooks_on_all_files(nox_session: Session) -> None:
         None.
 
     """
-    install_group_dependencies(
-        nox_session=nox_session,
-        groups=["pre-commit", "notebook", "testing"],
-    )
+    nox_session.install(".")
     args = nox_session.posargs or ["run", "--all-files", "--show-diff-on-failure"]
     nox_session.run("pre-commit", *args, external=True)
 
@@ -89,7 +48,7 @@ def run_pytest_suite(nox_session: Session) -> None:
         None.
 
     """
-    install_group_dependencies(nox_session=nox_session, groups=["testing"])
+    nox_session.install(".")
     args = nox_session.posargs or []
     nox_session.run("pytest", *args, success_codes=[0, 5], external=True)
 
@@ -110,7 +69,7 @@ def build_and_test_sphinx_documentation(nox_session: Session, builder: str) -> N
         None.
 
     """
-    install_group_dependencies(nox_session=nox_session, groups=["docs"])
+    nox_session.install(".")
     docs_build_directory = os.path.join(nox_session.env["TMPDIR"], "docs/_build")
     args = nox_session.posargs or ["docs", docs_build_directory]
     nox_session.run("sphinx-build", "-b", builder, *args, external=True)
