91b9ed1d9605af3b278b701561e9372a2b526c62
ESKYoung <ESKYoung@users.noreply.github.com>
2024-08-14 17:05:24 +0100

    test: add unit tests for the example exploratory package

---
+++ {{ cookiecutter.repository_name }}/exploratory/example/tests/example_package/test___init__.py
@@ -0,0 +1,21 @@
+"""Tests for the ``exploratory.example.src.example_package`` package."""
+
+import importlib.util
+import sys
+from importlib._bootstrap import ModuleSpec
+from pathlib import Path
+
+# Load the package by its full path, as it is not directly installed in the exploratory
+# Python environment
+spec: ModuleSpec = importlib.util.spec_from_file_location(
+    "example_package",
+    Path(__file__).parents[2].joinpath("src", "example_package", "__init__.py"),
+)
+example_package = importlib.util.module_from_spec(spec)
+sys.modules["example_package"] = example_package
+spec.loader.exec_module(example_package)
+
+
+def test___version___is_correctly() -> None:
+    """Test the ``__version__`` attribute is correct."""
+    assert example_package.__version__ == "0.1.0"
